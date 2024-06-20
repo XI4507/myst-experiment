b7bd7691a26647543269829ac605b0e8fc2c0b39
Rakesh Kumar <51470664+XI4507@users.noreply.github.com>
2024-06-20 15:57:29 +0530

    myst exp

---
+++ src/pages/MystPreview.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import type { NodeRenderer } from "@myst-theme/providers";
+import { mystParse } from "myst-parser";
+import { MyST, DEFAULT_RENDERERS } from "myst-to-react";
+
+import BASIC_RENDERERS from "./basic";
+import type { GenericParent, GenericNode } from "myst-common";
+import {
+  ReferencesProvider,
+  ThemeProvider,
+  Theme,
+} from "@myst-theme/providers";
+
+export const MY_RENDERERS: Record<string, NodeRenderer> = {
+  ...DEFAULT_RENDERERS,
+  text({ node }) {
+    // Change zero-width space into `<wbr>` which is better for copying
+    // These are used in links, and potentially other long words
+    if (!node.value?.includes('​')) {
+      return <>{node.value}</>;
+    }
+    const text = node.value.split('​');
+    return (
+      <>
+        {text.map((v, i) => (
+          <React.Fragment key={i}>
+            {v}
+            {i < text.length - 1 && <wbr />}
+          </React.Fragment>
+        ))}
+      </>
+    );
+  },
+};
+
+const MystPreview = ({ content }: { content: string }) => {
+  const [astNodes, setAstNodes] = useState<GenericNode | undefined>();
+  const MYST_THEME = Theme.light;
+  useEffect(() => {
+    const mdast: GenericParent = mystParse(content);
+    console.log(mdast);
+    setAstNodes(mdast);
+  }, [content]);
+  return (
+    <ThemeProvider renderers={MY_RENDERERS} theme={MYST_THEME}>
+      <MyST ast={astNodes} />
+    </ThemeProvider>
+  );
+};
+
+export default MystPreview;
